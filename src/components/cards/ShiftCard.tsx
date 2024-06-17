bfcd9255d5550cef30320c3e57a2e5e02bbd7652
John Nkp <john.nkp1@gmail.com>
2024-06-17 19:42:34 +1000

    archive functionality added to UI

--- src/components/cards/ShiftCard.tsx
+++ src/components/cards/ShiftCard.tsx
@@ -37,10 +37,8 @@ const ShiftCard: React.FC<Props>  = ({obj, todaysDate, toggleDelete, setToggleDe
         })
     }
 
-    // ${obj?.date === todaysDate? '!bg-[#D2F8D9] !border-green-500' : "bg-[#F9F9FB] border-gray-200"}`}
-
     return (
-        Array.isArray(obj.task) && obj.task?.length > 0? obj.task?.map((task: any, index) => (
+        obj && Array.isArray(obj.task) && obj.task?.length > 0? obj.task?.map((task: any, index) => (
             <div key={index + task.title} 
                 className={`relative cursor-pointer border ${task ? 'border-gray-300' : 'border-gray-200'} flex-col mt-2 text-[12px] h-[120px] w-28 rounded-md  p-1 
                     ${obj?.date === todaysDate? 'bg-[#DBEAFE] border-[#60A5FA]' : "bg-[#F9F9FB] border-gray-200"}
